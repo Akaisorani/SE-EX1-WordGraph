40d804a3ffceba0e3d9758ea89fc4d2a6f6da671
LBHong <2427462032@qq.com>
2017-10-20 21:43:13 +0800

    final

--- Lab4/src/Main.java
+++ Lab4/src/Main.java
@@ -1,18 +1,17 @@
 
 import java.awt.EventQueue;
 
-public class Main
-{
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					NewJFrame frame = new NewJFrame();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+public class Main{
+public static void main(String[] args) {
+    EventQueue.invokeLater(new Runnable() {
+        public void run() {
+            try {
+                NewJFrame frame = new NewJFrame();
+                frame.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    });
+}
 }
